500aa121adf90ab0d8c364afe2e201b7e6496128
Brad McCollum <btmcco8@gmail.com>
2018-08-27 20:41:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,6 @@ class Person
                 :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send (("#{key}="), value)}
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
